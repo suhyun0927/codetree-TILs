9f7c43eb6600f9cd364f5812641f5ad399994125
강수현 <127826893+suhyun0927@users.noreply.github.com>
2024-07-03 15:50:04 +0900

    Title: 연결리스트 1, Time: 13ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 240703/연결리스트 1/linked-list1.cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct Node{
+    string str;
+    Node* pre;
+    Node* next;
+};
+
+void printCur(Node* cur) {
+    if (cur->pre == NULL) cout << "(Null) ";
+    else cout << cur->pre->str << " ";
+
+    cout << cur->str << " ";
+
+    if (cur->next == NULL) cout << "(Null)\n";
+    else cout << cur->next->str << "\n";
+}
+
+int main() {
+    
+    // S_init
+    string init;
+    cin >> init;
+
+    Node* cur= new Node();
+    cur->str = init;
+
+    int N;
+    cin >> N;
+
+    for (int i = 0; i < N; i++) {
+        int num;
+        cin >> num;
+
+        if (num == 1) {
+            string tmp;
+            cin >> tmp;
+
+            Node* n2 = new Node();
+            n2->str = tmp;
+
+            if (cur->pre == NULL) {
+                n2->next = cur;
+                cur->pre = n2;
+            }
+            else {
+                n2->pre = cur->pre;
+                cur->pre->next = n2;
+                cur->pre = n2;
+                n2->next = cur;
+            }
+            
+            printCur(cur);
+        }
+        else if (num == 2) {
+            string tmp;
+            cin >> tmp;
+
+            Node* n2 = new Node();
+            n2->str = tmp;
+
+            if (cur->next == NULL) {
+                n2->pre = cur;
+                cur->next = n2;
+            }
+            else {
+                n2->next = cur->next;
+                cur->next->pre = n2;
+                cur->next = n2;
+                n2->pre = cur;
+            }
+            
+            printCur(cur);
+        }
+        else if (num == 3) {
+            if (cur->pre != NULL) {
+                cur->next = cur;
+                cur->pre = cur->pre->pre;
+                cur = cur->pre->next;
+            }
+
+            printCur(cur);
+        }
+        else { // num == 4
+            if (cur->next != NULL) {
+                cur->next = cur->next->next;
+                cur->pre = cur;
+                cur = cur->next->pre;
+            }
+
+            printCur(cur);
+        }
+    }
+
+    return 0;
+}
